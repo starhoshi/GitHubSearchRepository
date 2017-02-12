345ce2fc14a5edf6c0bc5c9c31720eb305472e55
starhoshi <kensuke1751@gmail.com>
2017-02-12 15:57:51 +0900

    Initial Commit

---
+++ GitHubSearchRepository/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GitHubSearchRepository
+//
+//  Created by Kensuke Hoshikawa on 2017/02/12.
+//  Copyright © 2017年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
